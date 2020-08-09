1383aa269a89d10d07d0f1463cfa2035d6e6480b
William <william.pinto@lambda3.com.br>
2020-08-09 17:50:31 -0300

    Module 5_Creating Header to navigate between pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import HeaderComponent from "./common/header/HeaderPage";
 
 function App() {
   return (
     <div className="container-fluid">
+      <HeaderComponent />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
