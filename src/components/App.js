c4edeea64633ecb2ed1086360c83018702c21120
William <william.pinto@lambda3.com.br>
2020-08-09 17:55:04 -0300

    Module 5_Creating a PageNotFound and using Switch for switching between routes when it receive a request (the PageNotFound will be the default, as it is the last in the list of pages)

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import HeaderComponent from "./common/header/HeaderPage";
+import PageNotFound from "./notFound/PageNotFound";
 
 function App() {
   return (
     <div className="container-fluid">
       <HeaderComponent />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+      <Route />
     </div>
   );
 }
