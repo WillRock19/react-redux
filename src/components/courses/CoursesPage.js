dd0f349394182e9b506f4cabd07def794bb8e1a4
William <william.pinto@lambda3.com.br>
2020-08-19 20:41:37 -0300

    Module 9_Creating a form and using bind to associate the handleChange function context to the class

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  }
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange.bind(this)} // Without this bind, the "this" context inside the fuynction will be undefined. With this, we are binding the function context to the class.
+          value={this.state.course.title}
+        />
+        <button>Submit</button>
+      </form>
+    );
   }
 }
 
