987bf91a0bc6b7f38cf1a40de16f00bd7991e83c
William <william.pinto@lambda3.com.br>
2020-08-19 20:44:09 -0300

    Module 9_ Using the binding in the constructor aproach to bind the handleChange class function

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -9,6 +9,9 @@ class CoursesPage extends React.Component {
         title: "",
       },
     };
+
+    this.handleChange = this.handleChange.bind(this); // The problem with the last commit aproach is that it will create a new function and associate it with the class context each time the component renders itself. That is a little too much.
+    //With this aproach (called "binding in the constructor"), the function is binding only once, not in every render.
   }
 
   handleChange(event) {
@@ -23,7 +26,7 @@ class CoursesPage extends React.Component {
         <h3>Add course</h3>
         <input
           type="text"
-          onChange={this.handleChange.bind(this)} // Without this bind, the "this" context inside the fuynction will be undefined. With this, we are binding the function context to the class.
+          onChange={this.handleChange}
           value={this.state.course.title}
         />
         <button>Submit</button>
