83c3473da96b24364c5f3597642a1ae1f5fc92c7
William <william.pinto@lambda3.com.br>
2020-08-19 20:48:45 -0300

    Module 9_Refactoring the coursesPage and removing it constructor and creating the state as a property, using the 'class field' aproach

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,18 +1,13 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      course: {
-        title: "",
-      },
-    };
-  }
+  state = {
+    course: {
+      title: "",
+    },
+  };
 
   handleChange = (event) => {
-    //Different from the later aproach, this is called "class fields". The arrow functions will inherit the binding context of their enclosing scope - in other words: the "this" keyword works with the class context;
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
   };
