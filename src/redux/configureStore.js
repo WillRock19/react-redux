baafe340e82b522a7d8e401fbc3b155f087b43c6
William <william.pinto@lambda3.com.br>
2020-08-19 21:55:40 -0300

    ]Module 9_Removing comments for legibility

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant"; // a middleware that will warn us if whe accidentally mutate any redux state
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 
 export default function configureStore(initialState) {
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //this line will add support to the redux devtools. The "__REDUX_DEVTOOLS_EXTENSION_COMPOSE__" is a unique global variable that says if the dev tools are avaiable at all
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant())) // Now we are gonna be able to interact with our redux store using the redux devtools in the browser
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
   );
 }
