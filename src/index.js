c3ec3ba585e26892d3349c75d03144c398b2a8a2
William <william.pinto@lambda3.com.br>
2020-08-19 21:59:48 -0300

    Module 9_Adding the Provider in the entrypoint and telling to redux what is the store that it will be using

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore(); //We could instantiate the initialState of the store and pass it here. If you're server rendering or initializing your redux store with data from a localStorage, it could be usefull. As we are not doing any of that, we won't initialize it.
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
