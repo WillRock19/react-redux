6b93e6be091f0a64d0d4a1098d60e38f0a19dda0
William <william.pinto@lambda3.com.br>
2020-08-19 21:55:02 -0300

    Module 9_Configuring the dev tools so we can interact with the store using the devtools from the browser

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant"; // a middleware that will warn us if whe accidentally mutate any redux state
 
 export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //this line will add support to the redux devtools. The "__REDUX_DEVTOOLS_EXTENSION_COMPOSE__" is a unique global variable that says if the dev tools are avaiable at all
+
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariant())
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant())) // Now we are gonna be able to interact with our redux store using the redux devtools in the browser
   );
 }
