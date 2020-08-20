2b598c508e6b2589f6ae3684cf4d3277cb09753b
William <william.pinto@lambda3.com.br>
2020-08-19 21:51:00 -0300

    Module 9_ Adding reduxImmutableStateInvariant as a middleware to the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant"; // a middleware that will warn us if whe accidentally mutate any redux state
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
 }
