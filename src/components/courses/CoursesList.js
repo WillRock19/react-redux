7ecca983d0544426c2e20fff010d79a1700befb3
William <william.pinto@lambda3.com.br>
2020-08-25 20:20:01 -0300

    Removing comments for clarity

--- src/components/courses/CoursesList.js
+++ src/components/courses/CoursesList.js
@@ -2,9 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const CoursesList = (
-  { courses } //Destructuring props on the parameter receiving, is equivalent to create a const courses = props.courses
-) => (
+const CoursesList = ({ courses }) => (
   <table>
     <thead>
       <tr>
