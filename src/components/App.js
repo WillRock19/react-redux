0345ab1eaab86a4937b995ffb9c91c5e8798d1fd
William <william.pinto@lambda3.com.br>
2020-08-27 19:09:35 -0300

    Creating ManageCoursePage component; adding it to switch (it must came before the /courses/ url, since once the switch find a match, it will end and get it's valur (and the :slug is just a parameter, not a url)

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./about/AboutPage";
 import HeaderComponent from "./common/header/HeaderPage";
 import PageNotFound from "./notFound/PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/courses/:slug" component={ManageCoursePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route component={PageNotFound} />
       </Switch>
