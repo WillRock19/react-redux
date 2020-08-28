93d6561031db0cbbf8a07f3f25750bea585646f3
William <william.pinto@lambda3.com.br>
2020-08-28 20:03:01 -0300

    MODULE 11_Updating the reducer to handle with create and update

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,8 +3,13 @@ import initialState from "./initialState";
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      //Map will return a new array, so I'm basically replacing the element with the matching course.id.
+      return state.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
