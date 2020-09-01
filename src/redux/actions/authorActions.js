9710bef9fe0c8f60204f28acb3119694fe41f06a
William <william.pinto@lambda3.com.br>
2020-09-01 19:52:39 -0300

    MODULE 12_Adding dispatch apicallError to authors action

--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -1,6 +1,6 @@
 import * as types from "./actionTypes";
 import * as authorApi from "../../api/authorApi";
-import { beginApiCall } from "./apiStatusActions";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
 
 export function loadAuthorSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -15,6 +15,7 @@ export function loadAuthors() {
         dispatch(loadAuthorSuccess(courses));
       })
       .catch((error) => {
+        dispatch(apiCallError());
         throw error;
       });
   };
