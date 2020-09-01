9589ba2879427dc46d3dbb5fa373cf0468a45eab
William <william.pinto@lambda3.com.br>
2020-09-01 19:32:18 -0300

    MODULE12_Adding form submition feedback

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -18,6 +18,7 @@ function ManageCoursePage({
 }) {
   const [course, setCourse] = useState({ ...props.course });
   const [errors, setErrors] = useState({});
+  const [saving, setSaving] = useState(false);
 
   useEffect(() => {
     if (courses.length === 0) {
@@ -46,6 +47,7 @@ function ManageCoursePage({
 
   function handleSave(event) {
     event.preventDefault();
+    setSaving(true);
     saveCourse(course).then(() => {
       history.push("/courses");
     });
@@ -61,6 +63,7 @@ function ManageCoursePage({
         authors={authors}
         onChange={handleChange}
         onSave={handleSave}
+        saving={saving}
       ></CourseForm>
     </>
   );
